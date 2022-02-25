f4a154e2230b9f66fb2775832fdd1e50b0772fb0
angel <dehainaut.angelique@orange.fr>
2022-02-25 16:50:32 +0100

    ->4

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <?php
 
 $servername = 'localhost';
-$username = '';
+$username = 'root';
 $password = '';
-$dbname = '';
+$dbname = 'eleves_wk';
 
-?>
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname,charset=utf8",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION).
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+}
+catch (PDOException $exception) {
+    die("erreur: " .$exception->getMessage());
+}
+
+$request = $conn->prepare("
+    SELECT el.prenom,el.nom,el.login,el.password,info.rue,info.cp,info.ville,info.pays
+    FROM eleve as el
+    INNER JOIN eleve_information as info ON el.information_id = info.id
+");
+
+$request->execute();
+echo "<pre";
+print_r($request->fetchAll());
+echo "</pre>";
+
+
